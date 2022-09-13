e23276e8f3cc40bfa1a35924353befe16f684357
inventedbypeter <inventedbypeter@gmail.com>
2022-09-13 19:50:00 +0900

    Create pill-shape item component using CSS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import Header from "./components/Header";
 import Item from "./components/Item";
+import styled from "styled-components";
+
+const AppWrapper = styled.div`
+  background-color: #fdfdfd;
+  width: 100%;
+  height: 100vh;
+`;
 
 function App() {
   return (
-    <>
+    <AppWrapper>
       <Header />
       <Item />
-    </>
+    </AppWrapper>
   );
 }
 
--- src/components/Item.js
+++ src/components/Item.js
@@ -4,10 +4,22 @@ const ItemWrapper = styled.div`
   width: 100%;
   height: 50px;
   border: 1px solid black;
+  border-radius: 999px;
+  display: flex;
+  align-items: center;
+  box-shadow: 0px 5px 2px 2px rgba(0, 0, 0, 0.1);
+
+  .text {
+    margin-left: 30px;
+  }
 `;
 
 function Item() {
-  return <ItemWrapper>Hello!</ItemWrapper>;
+  return (
+    <ItemWrapper>
+      <div className="text">Hello!</div>
+    </ItemWrapper>
+  );
 }
 
 export default Item;
